99300abfc0fee11fb4d15ca369ff69c87fbeb20a
kalz2q <katsu95i@gmail.com>
2022-09-11 15:45:14 +0900

    mm

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    document.title = "react-modal-window-dot"
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="open">
+        詳細を見る
+      </div>
+
+      <div className="mask" ></div >
+
+      <section className="modal" >
+        <p>こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。こんにちは。</p>
+        <div className="close" >
+          閉じる
+        </div >
+      </section >
+    </div >
   );
 }
 
